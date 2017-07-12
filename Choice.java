dc61166d8ca89acd76d8b77740cee7d56fe2cefc
Paweł Łysik <pawel.lysik@onet.pl>
2017-07-12 20:12:24 +0200

    Update and rename Wybór.java to Choice.java

--- Choice.java
+++ Choice.java
@@ -1,17 +1,17 @@
-package paczka;
+package game;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-class Wybór extends JPanel
+class Choice extends JPanel
 {
-	ArrayList<JButton> przyciski = new ArrayList<JButton>();
-	Wybór(String[] titles)
+	ArrayList<JButton> buttons = new ArrayList<JButton>();
+	Choice(String[] titles)
 	{
 		setLayout(new GridLayout(titles.length,1));
 		for(String s: titles)
-			przyciski.add(new JButton(s));
-		for(JButton jb: przyciski)
+			buttons.add(new JButton(s));
+		for(JButton jb: buttons)
 			add(jb);
 	}
 }
