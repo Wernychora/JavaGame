80f93f98fd8eb0e6f151572fcc1c5735e2f267b4
Paweł Łysik <pawel.lysik@onet.pl>
2017-07-12 19:38:58 +0200

    Update PierwszaKlasa.java

--- PierwszaKlasa.java
+++ PierwszaKlasa.java
@@ -1,4 +1,4 @@
-package paczka;
+package game;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
@@ -7,55 +7,55 @@ import java.util.TimerTask;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
-public class PierwszaKlasa extends JFrame 
+public class MainClass extends JFrame 
 {
 	static MyPanel panel;
-	static Twarz twarz;
+	static Face face;
 	java.util.Timer timer;
 	KeyListener kl = new KeyListener()
 	{
 		
 		public void keyPressed(KeyEvent e)
 		{
-			panel.klawisz(e);
+			panel.key(e);
 		}
 		public void keyReleased(KeyEvent e){}
 		public void keyTyped(KeyEvent e){}
 	};
-	void koniec(String info1, String info2)
+	void end(String info1, String info2)
 	{
-		panel.przegrana=false;
-		panel.wygrana=false;
+		panel.defeat=false;
+		panel.victory=false;
 		panel.dead=1000000000;
-		int opcja=JOptionPane.showConfirmDialog(null, info1+", chcesz zagraæ jeszcze raz?", info2, JOptionPane.YES_NO_OPTION);
-		if(opcja==JOptionPane.YES_OPTION)
+		int option=JOptionPane.showConfirmDialog(null, info1+", do you want play again?", info2, JOptionPane.YES_NO_OPTION);
+		if(option==JOptionPane.YES_OPTION)
 		{
 			timer.cancel();
-			graj(panel.k);
+			play(panel.k);
 		}
-		if(opcja==JOptionPane.NO_OPTION)
+		if(option==JOptionPane.NO_OPTION)
 		{
-			twarz.gra=false;
+			face.game=false;
 			panel.setVisible(false);
-			twarz.setVisible(true);
+			face.setVisible(true);
 			timer.cancel();
 		}
 	}
-	public PierwszaKlasa() 
+	public MainClass() 
 	{
 		super("Rysowanie");
-		twarz = new Twarz(this);
+		face = new Face(this);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(screenSize.width/2-400, screenSize.height/2-300);		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		setLayout(null);
-		twarz.setBounds(0,0,800,600);
-		add(twarz);
+		face.setBounds(0,0,800,600);
+		add(face);
 		setSize(800, 600);
 		addKeyListener(kl);
 	}
-	void graj(Konfiguracja k)
+	void game(Configuration k)
 	{
 		panel = new MyPanel(k);
 		panel.setBounds(0,0,800,600);
@@ -66,12 +66,12 @@ public class PierwszaKlasa extends JFrame
 		{
 			public void run()
 			{
-				twarz.setVisible(false);
+				face.setVisible(false);
 				panel.setVisible(true);
-				if(!panel.wygrana && !panel.przegrana)panel.time++;
+				if(!panel.victory && !panel.default)panel.time++;
 				panel.repaint();
-				if(panel.przegrana)koniec("Przegra³eœ","Przegrana");
-				if(panel.wygrana)koniec("Wygra³eœ","Wygrana");
+				if(panel.default)koniec("You lose","Defeat");
+				if(panel.victory)koniec("You won","Victory");
 			}
 		};
 		timer.schedule(tt, 0, 5);
@@ -82,7 +82,7 @@ public class PierwszaKlasa extends JFrame
 		{
 			public void run()
 			{
-				new PierwszaKlasa();
+				new MainClass();
 			}
 		});
 	}
